15b2f527c5ce712defdb81126e1650145439e6d5
Ganesh Raj Manandhar <ganesh.manandhar@ekbana.info>
2023-09-01 16:47:52 +0545

    chore: next-auth ongoing

---
+++ lib/session.ts
@@ -0,0 +1,37 @@
+import { getServerSession } from "next-auth/next";
+import { NextAuthOptions, User } from "next-auth";
+import { AdapterUser } from "next-auth/adapters";
+import GoogleProvider from "next-auth/providers/google";
+import jsonwebtoken from "jsonwebtoken";
+import { JWT } from "next-auth/jwt";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  jwt: {
+    encode: ({ secret, token }) => {
+      const encodedToken = jsonwebtoken.sign(
+        {
+          ...token,
+          iss: "grafbase",
+          exp: Math.floor(Date.now() / 1000) + 60 * 60,
+        },
+        secret
+      );
+
+      return encodedToken;
+    },
+    decode: async ({ secret, token }) => {
+      const decodedToken = jsonwebtoken.verify(token!, secret);
+      return decodedToken as JWT;
+    },
+  },
+  theme: {
+    colorScheme: "light",
+    logo: "/logo.svg",
+  },
+};
